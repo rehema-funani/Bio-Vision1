64b826feeb78e2658c8cb3028f718d93015d50fb
Rehema Funani <rehemafunani.com>
2025-07-30 02:18:13 +0300

    adds files

--- components/HeaderWrapper.tsx
+++ components/HeaderWrapper.tsx
@@ -7,7 +7,7 @@ import { Header } from "@/components/Header";
 export default function HeaderWrapper() {
   const pathname = usePathname();
 
-  const lightRoutes = ["/mission", "/shop", "/contact","/surveys","/outlets","/resources","/contact"];
+  const lightRoutes = ["/mission", "/shop", "/contact","/surveys","/outlets","/resources","/contact","/events"];
   const isLight = lightRoutes.includes(pathname);
 
   return <Header theme={isLight ? "light" : "dark"} />;
